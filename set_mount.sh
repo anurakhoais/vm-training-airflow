eaa40d060d31748a73aa70ccdae255cf8b589e3a
Anurak Homkaew <anurakho@ais.co.th>
2024-11-05 09:53:19 +0700

    Create set_mount.sh

---
+++ set_mount.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install cifs-utils
+
+RESOURCE_GROUP_NAME="rg-AirflowTraining-az-asse-dev-001"
+STORAGE_ACCOUNT_NAME="stairflowtrain"
+
+# This command assumes you have logged in with az login
+HTTP_ENDPOINT=$(az storage account show \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $STORAGE_ACCOUNT_NAME \
+    --query "primaryEndpoints.file" --output tsv | tr -d '"')
+SMBPATH=$(echo $HTTP_ENDPOINT | cut -c7-${#HTTP_ENDPOINT})
+FILE_HOST=$(echo $SMBPATH | tr -d "/")
+
+nc -zvw3 $FILE_HOST 445
