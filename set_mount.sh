de956ecf8ae7008d5102d788bfc2243e0fb1ae67
Anurak Homkaew <anurakho@ais.co.th>
2024-11-05 15:53:09 +0700

    Update set_mount.sh

--- set_mount.sh
+++ set_mount.sh
@@ -1,5 +1,13 @@
 sudo apt-get install cifs-utils
-
 sudo mkdir /mnt/training
-sudo mount -t cifs //stairflowtrain.blob.core.windows.net/training /mnt/training -o vers=3.0,username=stairflowtrain,password=sQDBkgdtxvuJx3olCaS9DDaNxulb7kJLdKPLM1X9WPnQ2otV/7MBmOyslT+hqfqQLEBL1Z/bEoSR+ASt3t79kw==,dir_mode=0777,file_mode=0777
-df -h /mnt/training
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/stairflowtrain.cred" ]; then
+    sudo bash -c 'echo "username=stairflowtrain" >> /etc/smbcredentials/stairflowtrain.cred'
+    sudo bash -c 'echo "password=sQDBkgdtxvuJx3olCaS9DDaNxulb7kJLdKPLM1X9WPnQ2otV/7MBmOyslT+hqfqQLEBL1Z/bEoSR+ASt3t79kw==" >> /etc/smbcredentials/stairflowtrain.cred'
+fi
+sudo chmod 600 /etc/smbcredentials/stairflowtrain.cred
+
+sudo bash -c 'echo "//stairflowtrain.file.core.windows.net/training /mnt/training cifs nofail,credentials=/etc/smbcredentials/stairflowtrain.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
+sudo mount -t cifs //stairflowtrain.file.core.windows.net/training /mnt/training -o credentials=/etc/smbcredentials/stairflowtrain.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
