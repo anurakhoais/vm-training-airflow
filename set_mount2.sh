4b13f7c62b67a53ee916d1abb746e87cb4856894
Anurak Homkaew <anurakho@ais.co.th>
2024-11-26 13:45:24 +0700

    Update set_mount2.sh

--- set_mount2.sh
+++ set_mount2.sh
@@ -1,5 +1,5 @@
 sudo apt-get install cifs-utils
 
 sudo mkdir /mnt/training
-sudo mount -t cifs //stairflowtrain.blob.core.windows.net/training /mnt/training -o vers=3.0,username=stairflowtrain,password=sQDBkgdtxvuJx3olCaS9DDaNxulb7kJLdKPLM1X9WPnQ2otV/7MBmOyslT+hqfqQLEBL1Z/bEoSR+ASt3t79kw==,dir_mode=0777,file_mode=0777
+sudo mount -t cifs //stairflowtrain.blob.core.windows.net/training /mnt/training -o vers=3.0,username=stairflowtrain,password=<key>,dir_mode=0777,file_mode=0777
 df -h /mnt/training
